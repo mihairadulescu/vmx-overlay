bafb1c2fcc2a738d7b4e84e68fc18e35b1810e3e
Mihai Radulescu <mihai.radulescu@nerds.sh>
2024-05-27 13:36:52 +0300

    [] Update ws connection

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import WebSocketComponent from "./WebSocketComponent";
 export default function Home() {
   return (
     <main className="bg-transparent">
-      <WebSocketComponent wsUrl="ws://localhost:3000/ws" />
+      <WebSocketComponent wsUrl="ws://geke.hermannstadtpfarrkirche.online/ws" />
     </main>
   );
 }
