890b294d9eaedb000a12e6aacbd4c1ca66828cef
Mihai Radulescu <mihai.radulescu@nerds.sh>
2024-08-26 12:33:15 +0300

    [] Statie

--- src/app/registration/presenter.js
+++ src/app/registration/presenter.js
@@ -6,22 +6,10 @@ const Presenter = React.forwardRef(({ data }, ref) => (
     className="border border-gray-300 rounded-lg p-4 flex flex-col items-center overflow-hidden bg-white"
     style={{ width: '4in', height: '3in', color: "black" }}
   >
-    {/* Event Logo */}
-    <div className="flex items-center justify-center mb-4">
-      <img src="/event-logo.png" alt="Event Logo" style={{ width: 'auto', height: '1in' }} />
-    </div>
 
     {/* QR Code and Details */}
     <div className="flex flex-row items-center w-full">
-      {/* Name, Title, City, Country */}
-      <div className="flex flex-col items-start text-left px-4 w-1/2">
-        <div className="text-lg font-bold">
-          <span>{data.name}</span>
-        </div>
-        <div className="text-md mt-1">
-          {data.organisation}
-        </div>
-      </div>
+     
 
       {/* QR Code */}
       {data.qrCode && (
